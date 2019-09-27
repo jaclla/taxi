a65d0d5a6e11170ac9dd62e357df3eb14f85e1ac
jaclla <654192670@qq.com>
2019-09-27 12:09:39 +0800

    整合前端页面

--- src/main/java/com/logic/taxi/controller/UserInfoController.java
+++ src/main/java/com/logic/taxi/controller/UserInfoController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("userInfo")
+@RequestMapping("api")
 @Api(tags = {"用户操作接口"}, description = "userInfoControler")
 public class UserInfoController {
 
---
+++ src/main/resources/static/precache-manifest.48c0f07528fb570a678181e15a20b872.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "2e2f57eb4c7f9965c32c",
+    "url": "/public/js/chunk-vendors.79ba2f03.js"
+  },
+  {
+    "revision": "1af72eee0f4a24ef9e21",
+    "url": "/public/js/app.85bb36ee.js"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/public/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/public/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "2e2f57eb4c7f9965c32c",
+    "url": "/public/css/chunk-vendors.1c694d0b.css"
+  },
+  {
+    "revision": "1af72eee0f4a24ef9e21",
+    "url": "/public/css/app.dc8c7322.css"
+  },
+  {
+    "revision": "e5634054745837d6a77f99f5cff4388d",
+    "url": "/index.html"
+  }
+];
